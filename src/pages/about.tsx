63d0b71baeb60aeac1dd575e3f5bb0a3e5abe444
Dennis Moes <dennismoes@me.com>
2023-04-14 12:02:31 +0200

    Some more incremental changes to the design

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import ProjectCard from '@/components/ui/project-card'
-import Image from 'next/image'
+
+import ProjectData from '@/data/projects.json'
 
 import MeImage from '../../public/images/me-explaining.jpeg'
 import MeCoding from '../../public/svg/me-coding.svg'
@@ -24,7 +25,7 @@ export default function About() {
             <p>
               I love working on my{' '}
               <HoverableLink
-                href="projects"
+                href="/projects"
                 label="side projects"
                 newTab={false}
               />
@@ -34,16 +35,14 @@ export default function About() {
             </p>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <ProjectCard
-              icon="📚"
-              title="Journal"
-              description="Beautiful journal app for macOS with over 1K users"
-            />
-            <ProjectCard
-              icon="💈"
-              title="Rioll"
-              description="3D printed and IoT kits for makers"
-            />
+            {ProjectData.map((project, index) => (
+              <ProjectCard
+                key={index}
+                icon={project.icon}
+                title={project.title}
+                description={project.description}
+              />
+            ))}
           </div>
           <ElongatedImage
             image={MeImage}
@@ -54,8 +53,13 @@ export default function About() {
               <p>
                 Hello! My name is Dennis and I&apos;m a software engineering
                 student who is based in the picturesque Amsterdam. I am
-                currently studying <HoverableLink href="#" label="HvA" /> for my
-                Bachelor.
+                currently studying{' '}
+                <HoverableLink
+                  href="https://www.hva.nl"
+                  newTab={true}
+                  label="HvA"
+                />{' '}
+                for my Bachelor.
               </p>
               <p>
                 I have always been inspired by the tech industry, computers and
@@ -63,8 +67,12 @@ export default function About() {
                 got dared by a classmate to make a mobile app, I discovered that
                 coding is also a big passion of mine. Fast forward to today and
                 I have graduated{' '}
-                <HoverableLink href="#" label="Regio College" /> as a Software
-                Engineer.
+                <HoverableLink
+                  href="https://www.regiocollege.nl/"
+                  newTab={true}
+                  label="Regio College"
+                />{' '}
+                as a Software Engineer.
               </p>
               <p>
                 In my spare time I always try to tinker on some side projects
@@ -85,7 +93,7 @@ export default function About() {
             isFullSize={false}
             darkMode={true}
           />
-          <div className="space-y-8">
+          {/* <div className="space-y-8">
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
               dolorum quis pariatur, blanditiis dolorem et non mollitia aliquid
@@ -104,7 +112,7 @@ export default function About() {
               saepe dolorem dicta quisquam perspiciatis reiciendis adipisci,
               quae id sed necessitatibus.
             </p>
-          </div>
+          </div> */}
         </section>
       </main>
     </>
