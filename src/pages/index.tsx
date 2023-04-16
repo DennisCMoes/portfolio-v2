d48fd972c91405646da9c4d2b6af1a3a4482cc0f
Dennis Moes <dennismoes@me.com>
2023-04-16 19:07:21 +0200

    Small changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,23 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import SkillCard from '@/components/ui/skill-card'
-import ToolCard from '@/components/ui/tool-card'
-import WorkExperience from '@/components/ui/work-experience'
 
+import { Inter } from 'next/font/google'
+import { useRef } from 'react'
+
+import WorkExperience from '@/components/ui/work-experience'
 import WorkExperienceList from '@/data/work.json'
 
 const inter = Inter({ subsets: ['latin'], weight: '400' })
 
-type Experience = {
-  title: string
-  description: string
-  company: string
-  start: string
-  end: string
-  tags: string[]
-}
-
-// TODO: Work on OG:tags and seo optimization
 export default function Home() {
+  const secondSection = useRef<HTMLElement>(null)
+
+  function scrollToSecondSection() {
+    const positionY =
+      secondSection.current!.getBoundingClientRect().top + window.screenY + -100
+    window.scrollTo({ top: positionY, behavior: 'smooth' })
+  }
+
   return (
     <>
       <Head>
@@ -63,30 +61,53 @@ export default function Home() {
               computer applications in Amsterdam. I spend most of my time
               building cool things (and sometimes making them look pretty too).
             </p>
-            <Link
-              href="/about"
-              className="flex items-center bg-secondary-light text-primary-light py-4 px-12 md:hover:px-14 transition-all max-w-fit rounded-md"
-            >
-              Read my bio
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 ml-2 -mr-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+            <div className="flex flex-row gap-2">
+              <button
+                onClick={() => scrollToSecondSection()}
+                className="flex items-center bg-secondary-light text-primary-light p-4 transition-all md:hover:px-6 max-w-fit rounded-md"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </Link>
+                <svg
+                  aria-hidden="true"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </svg>
+              </button>
+              <Link
+                href="/about"
+                className="flex items-center bg-secondary-light text-primary-light py-4 px-12 md:hover:px-14 transition-all max-w-fit rounded-md"
+              >
+                Read my bio
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 ml-2 -mr-1"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </Link>
+            </div>
           </div>
         </section>
         <section
           id="work-experience"
-          className="min-h-screen max-w-4xl mb-32 mx-auto space-y-12 text-2xl md:text-2xl"
+          ref={secondSection}
+          className="min-h-screen max-w-4xl mb-32 mx-auto space-y-12 text-2xl md:text-2xl px-4"
         >
           <div>
             <p>Work experience</p>
@@ -96,7 +117,7 @@ export default function Home() {
             </p>
           </div>
           <div className="inline-flex flex-col w-full space-y-8">
-            {WorkExperienceList.map((work: any, index) => (
+            {WorkExperienceList.map((work: any, index: number) => (
               <WorkExperience key={index} {...work} />
             ))}
           </div>
