c84dad0febf69283bd47c0395e58edb63a3dc8a1
Dennis Moes <dennismoes@me.com>
2023-06-15 15:56:44 +0200

    Changed the size of the grid

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -24,63 +24,20 @@ async function fetchProjects(): Promise<ProjectMetaData[]> {
 export default async function Projects() {
   const projects: ProjectMetaData[] = await fetchProjects()
 
-  // TODO: Fix the sorting
   const sortArray = (a: any, b: any) => {
     return new Date(a.date).valueOf() - new Date(b.date).valueOf()
   }
 
-  const divideArray = (index: number, arrayIndex: number) => {
-    switch (arrayIndex) {
-      case 0:
-        return index % 3 === 0
-      case 1:
-        return (index + 1) % 3 === 0
-      case 2:
-        return (index + 2) % 3 === 0
-      default:
-        return false
-    }
-  }
-
   return (
+    // TODO: Add mobile and large desktop support
     <div className="max-w-6xl mx-auto py-12 px-4">
       <p className="text-4xl font-semibold mb-4">Projects</p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="flex flex-col gap-4">
-          {/* <ProjectCard
-            project={{
-              coverImage: '',
-              date: '',
-              description: 'This is an intentional broken link',
-              repository: '',
-              slug: '2',
-              technologies: [],
-              title: 'Wrong page',
-            }}
-          /> */}
-          {projects
-            .sort(sortArray)
-            .filter((_, index) => divideArray(index, 0))
-            .map((project: ProjectMetaData, index: number) => (
-              <ProjectCard key={index} project={project} />
-            ))}
-        </div>
-        <div className="flex flex-col gap-4">
-          {projects
-            .sort(sortArray)
-            .filter((_, index) => divideArray(index, 1))
-            .map((project: ProjectMetaData, index: number) => (
-              <ProjectCard key={index} project={project} />
-            ))}
-        </div>
-        <div className="flex flex-col gap-4">
-          {projects
-            .sort(sortArray)
-            .filter((_, index) => divideArray(index, 2))
-            .map((project: ProjectMetaData, index: number) => (
-              <ProjectCard key={index} project={project} />
-            ))}
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+        {projects
+          .sort(sortArray)
+          .map((project: ProjectMetaData, index: number) => (
+            <ProjectCard key={index} project={project} />
+          ))}
       </div>
     </div>
   )
