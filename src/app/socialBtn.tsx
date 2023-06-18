f814ffb9837e3c91d5df26eafd081de70d865c3f
Dennis Moes <dennismoes@me.com>
2023-06-18 18:47:00 +0200

    Reworked the buttons

---
+++ src/app/socialBtn.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+
+type Props = {
+  label: string
+  children: React.ReactNode
+  href?: string
+}
+
+export default function SocialBtn({ label, children }: Props) {
+  const textElement = useRef<HTMLDivElement>(null)
+
+  const [isHovering, setIsHovering] = useState<boolean>(false)
+  const [isMobile, setIsMobile] = useState<boolean>(false)
+  const [width, setWidth] = useState<number>(0)
+
+  const handleEnter = () => setIsHovering(true)
+  const handleLeave = () => setIsHovering(false)
+
+  useEffect(() => {
+    if (!textElement.current) return
+
+    setWidth(textElement.current!.offsetWidth)
+  }, [textElement])
+
+  useEffect(() => {
+    // 768 is medium screen width
+    setIsMobile(screen.width < 768)
+  }, [width])
+
+  return (
+    <div
+      onMouseEnter={handleEnter}
+      onMouseLeave={handleLeave}
+      style={{ paddingRight: !isMobile && isHovering ? width + 20 : 16 }}
+      className="relative group bg-primary-light hover:bg-primary-dark text-primary-dark hover:text-primary-light rounded-md transition-all duration-300 flex flex-row p-4"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-6 h-6"
+        viewBox="0 0 24 24"
+        strokeWidth={2}
+        stroke="currentColor"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        {children}
+      </svg>
+      <div
+        ref={textElement}
+        className="md:absolute right-4 md:opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-300 delay-0 group-hover:delay-200"
+      >
+        <p>{label}</p>
+      </div>
+    </div>
+  )
+}
--- src/components/staticPages/home-page.tsx
+++ src/components/staticPages/home-page.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import WorkExperience from '@/components/ui/work-experience'
 import { WorkExperienceProps } from '@/types/props'
+import SocialBtn from '@/app/socialBtn'
 
 const inter = Inter({ subsets: ['latin'], weight: '400' })
 
@@ -18,6 +19,7 @@ export default function HomePage({
     // Get the scrollable position of the second section and add a negative offset
     const positionY =
       secondSection.current!.getBoundingClientRect().top + window.screenY - 100
+
     // Scroll to said position
     window.scrollTo({ top: positionY, behavior: 'smooth' })
   }
@@ -39,9 +41,58 @@ export default function HomePage({
             applications in Amsterdam. I spend most of my time building cool
             things (and sometimes making them look pretty too).
           </p>
-          {/* TODO: Change the hover animations so that it will flow out of the other side */}
-          <div className="flex flex-row gap-2">
-            <button
+          <div className="flex flex-row flex-wrap gap-2">
+            <div
+              className="cursor-pointer"
+              onClick={() => scrollToSecondSection()}
+            >
+              <SocialBtn label="Work">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M6 9l6 6l6 -6"></path>
+              </SocialBtn>
+            </div>
+            <Link
+              href="https://linkedin.com/in/dennismoes"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SocialBtn label="LinkedIn">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                <path d="M8 11l0 5"></path>
+                <path d="M8 8l0 .01"></path>
+                <path d="M12 16l0 -5"></path>
+                <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+              </SocialBtn>
+            </Link>
+            <Link
+              href="https://github.com/kingdennis-crypto"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <SocialBtn label="GitHub">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+              </SocialBtn>
+            </Link>
+            {/* <SocialBtn label="Projects">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z"></path>
+              <path d="M19 16h-12a2 2 0 0 0 -2 2"></path>
+              <path d="M9 8h6"></path>
+            </SocialBtn>
+            <Link href="/about">
+              <SocialBtn label="Bio">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M8 4h-2l-3 10"></path>
+                <path d="M16 4h2l3 10"></path>
+                <path d="M10 16l4 0"></path>
+                <path d="M21 16.5a3.5 3.5 0 0 1 -7 0v-2.5h7v2.5"></path>
+                <path d="M10 16.5a3.5 3.5 0 0 1 -7 0v-2.5h7v2.5"></path>
+              </SocialBtn>
+            </Link> */}
+
+            {/* <button
               onClick={() => scrollToSecondSection()}
               className="flex items-center bg-primary-dark text-primary-light dark:bg-primary-light dark:text-primary-dark p-4 transition-all md:hover:px-6 max-w-fit rounded-md"
             >
@@ -79,7 +130,7 @@ export default function HomePage({
                   clipRule="evenodd"
                 ></path>
               </svg>
-            </Link>
+            </Link> */}
           </div>
         </div>
       </section>
