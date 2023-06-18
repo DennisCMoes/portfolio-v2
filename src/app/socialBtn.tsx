21cdc2a357b4a4ce1abefb5ff35e2983d1c2df2d
Dennis Moes <dennismoes@me.com>
2023-06-18 18:52:19 +0200

    Fixed a buy by removing the pointer eventlistener

--- src/app/socialBtn.tsx
+++ src/app/socialBtn.tsx
@@ -50,7 +50,7 @@ export default function SocialBtn({ label, children }: Props) {
       </svg>
       <div
         ref={textElement}
-        className="md:absolute right-4 md:opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-300 delay-0 group-hover:delay-200"
+        className="pointer-events-none md:absolute right-4 md:opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-300 delay-0 group-hover:delay-200"
       >
         <p>{label}</p>
       </div>
