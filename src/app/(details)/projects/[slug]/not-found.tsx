8693f44209be2259f2305737a5a8ba017985cdf7
Dennis Moes <dennismoes@me.com>
2023-07-02 10:27:30 +0200

    Redesigned the not found page

--- src/app/(details)/projects/[slug]/not-found.tsx
+++ src/app/(details)/projects/[slug]/not-found.tsx
@@ -1,19 +1,24 @@
 import Link from 'next/link'
+import MeCoding from '@public/svg/me-coding.svg'
+import ElongatedImage from '@/components/ui/elongated-image'
 
 export default function NotFound() {
   return (
-    <div className="max-w-2xl mx-auto py-12">
-      <p className="text-4xl">Oops!</p>
-      <p className="text-2xl font-light">
-        Looks like the project you were looking for took a vacation. It&apos;s
-        enjoying the sun. But don&apos;t worry, we&apos;ll send out a search
-        party to bring it back. In the meantime, feel free to explore other
-        projects{' '}
-        <Link href="/projects" className="text-blue-600 underline">
-          here
-        </Link>{' '}
-        and make yourself at home!
-      </p>
-    </div>
+    <section>
+      <div className="max-w-2xl mx-auto text-center space-y-4">
+        <ElongatedImage image={MeCoding} isFullSize={false} darkMode={true} />
+        <p className="text-4xl">Oh no... We couldn&apos;t find this project</p>
+        <p className="text-xl">
+          We searched high and low for this project but we just couldn&apos;t
+          find what you were looking for. Let&apos;s get you to a better place.
+        </p>
+        <Link
+          href="/projects"
+          className="flex max-w-fit mx-auto bg-primary-light hover:bg-primary-dark text-primary-dark hover:text-primary-light rounded-md transition-all duration-300 py-2 px-6"
+        >
+          Back to Projects
+        </Link>
+      </div>
+    </section>
   )
 }
