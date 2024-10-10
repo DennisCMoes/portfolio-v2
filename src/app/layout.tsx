0c09a3a5c865d18c20d7fc8c47d9a9b084ad4dc0
Dennis Moes <dennismoes@me.com>
2024-10-10 16:39:54 +0200

    del: delete old code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,28 @@
 import '@/styles/globals.css'
+import '@radix-ui/themes/styles.css'
 
 import { Metadata } from 'next'
-
-import RootLayoutPage from '@/components/staticPages/rootLayout-page'
-import { Children } from '@/types'
+import { JetBrains_Mono } from 'next/font/google'
+import { ReactNode } from 'react'
+import { Theme } from '@radix-ui/themes'
 
 export const metadata: Metadata = {
   viewport: { width: 'device-width', initialScale: 1 },
   icons: { icon: '/favicon.ico', apple: '/favicon.ico' },
 }
 
-export default function RootLayout({ children }: Children) {
-  return <RootLayoutPage>{children}</RootLayoutPage>
+const jetbrains = JetBrains_Mono({ subsets: ['latin'] })
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Theme>
+          <div className={`bg-background min-h-screen ${jetbrains.className}`}>
+            <div className="max-w-2xl mx-auto py-24">{children}</div>
+          </div>
+        </Theme>
+      </body>
+    </html>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,61 @@
 import { Metadata } from 'next'
-import HomePage from '@/components/staticPages/home-page'
-
-// Data
-import WorkExperienceList from '@/data/work.json'
+import { IconMapPin } from '@tabler/icons-react'
+import Section from '@/components/section'
+import ExperienceCard from '@/components/experience'
+import Switch from '@/components/switch'
 
 export const metadata: Metadata = {
   title: 'Home',
   description: 'The index page of my personal website',
-  openGraph: {
-    type: 'website',
-    title: 'Landing page of portfolio',
-    url: 'https://dennismoes.com',
-    images: [{ url: 'https://dennismoes.com/images/pages/index-page.png' }],
-    description: 'This will be the index page to my portfolio',
-  },
 }
 
 export default function Home() {
-  return <HomePage workExperience={WorkExperienceList} />
+  return (
+    <div className="flex flex-col gap-8">
+      <div>
+        <div className="flex text-secondary">
+          <IconMapPin />
+          <p className="uppercase">Amsterdam</p>
+        </div>
+        <h1 className="uppercase text-5xl font-medium">
+          <span className="text-primary">Developer by day,</span>
+          <br />
+          <Switch />
+          <span className="ml-2 text-secondary">Jedi by night</span>
+        </h1>
+      </div>
+      <Section label="About">
+        <p className="font-medium">
+          Lorem ipsum ut dolore id sunt commodo amet qui, commodo sint laboris,
+          irure exercitation est, consequat. In ut, do elit nisi officia magna
+          mollit cupidatat cupidatat aliquip laboris sed commodo officia.
+          Commodo cillum ut velit sed qui sunt mollit ullamco nisi eu culpa anim
+          mollit consequat.
+        </p>
+      </Section>
+      <Section label="Experience" classNames="flex flex-col gap-4">
+        {workExperienceList.map((experience) => (
+          <ExperienceCard key={experience.title} {...experience} />
+        ))}
+      </Section>
+    </div>
+  )
 }
+
+const workExperienceList = [
+  {
+    title: 'ChipSoft',
+    subtitle: 'Wekstudent',
+    date: 'apr. 2023 - present',
+  },
+  {
+    title: 'Crafter',
+    subtitle: 'Junior Software Developer',
+    date: 'sep. 2022 - nov. 2022',
+  },
+  {
+    title: 'Next Gen Atlas',
+    subtitle: 'Co-founder',
+    date: 'apr. 2021 - jul. 2022',
+  },
+]
