0b76611d19d75812ee2f37f433d864894df64ea8
Dennis Moes <dennismoes@me.com>
2024-10-11 09:08:52 +0200

    feat: add a footer component
    In this commit there is a footer component made with links to my github and linkedin opened in new tabs

---
+++ src/components/footer.tsx
@@ -0,0 +1,36 @@
+import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react'
+import { NextFont } from 'next/dist/compiled/@next/font'
+import Link from 'next/link'
+
+type Props = { font: NextFont }
+
+export default function Footer({ font }: Props) {
+  return (
+    <footer
+      className={`flex flex-col items-center justify-center bg-footer py-16 ${font.className}`}
+    >
+      <div className="flex flex-row gap-2">
+        <Link
+          href={'https://www.linkedin.com/in/dennismoes/'}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded-md text-primary transition-all duration-200 hover:scale-95 hover:opacity-60"
+        >
+          <IconBrandLinkedin size={48} strokeWidth={1.7} />
+        </Link>
+
+        <Link
+          href={'https://github.com/DennisCMoes'}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded-md text-primary transition-all duration-200 hover:scale-95 hover:opacity-60"
+        >
+          <IconBrandGithub size={48} strokeWidth={1.7} />
+        </Link>
+      </div>
+      <p className="pt-2">
+        Copyright &copy; 2024 Dennis Moes. All rights reserved.
+      </p>
+    </footer>
+  )
+}
